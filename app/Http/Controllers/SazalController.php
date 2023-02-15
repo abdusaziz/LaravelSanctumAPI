50324f3f20d24bea193e5e9c3600ddb8255c0ac5
abdusaziz <abdusaziz@gmail.com>
2023-02-15 19:34:38 +0600

    Updated till TaskController index method is now working

---
+++ app/Http/Controllers/SazalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sazal;
+use Illuminate\Http\Request;
+
+class SazalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sazal  $sazal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sazal $sazal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sazal  $sazal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sazal $sazal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sazal  $sazal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sazal $sazal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sazal  $sazal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sazal $sazal)
+    {
+        //
+    }
+}
